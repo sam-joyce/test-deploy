ff59290c701432999060c25eacfd9dfd5fb5643c
Sam <samjoyce18@yahoo.com>
2022-12-05 15:15:07 +0000

    pushing all code

--- src/main/java/com/example/greeting/GreetingApplication.java
+++ src/main/java/com/example/greeting/GreetingApplication.java
@@ -2,12 +2,13 @@ package com.example.greeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @RestController
 public class GreetingApplication {
 	public static void main(String[] args) {
